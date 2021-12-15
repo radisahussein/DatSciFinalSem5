94953bba31996888f6a493ac9575845903cc48cf
radisahussein <56021204+radisahussein@users.noreply.github.com>
2021-12-15 14:45:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# DATASETS ====================================================================================
+
+df_1 = pd.read_csv(r'C:\Users\Fajar Rachmadi\Desktop\CODE\DatSciFinalSem5\Data\conflict-data-for-indonesia-2.csv')
+df_2 = pd.read_csv(r'C:\Users\Fajar Rachmadi\Desktop\CODE\DatSciFinalSem5\Data\conflict_data_idn2.csv')
+
+# PREPARING DATA ====================================================================================================
+
+df = pd.concat([df_1,df_2]).drop_duplicates().reset_index(drop=True)
+print(df.head(20))
+
+print(df.sort_values(by='year'))
