461ea1ec0378520c6f94227ce1b157e955f866e0
radisahussein <56021204+radisahussein@users.noreply.github.com>
2021-12-15 13:51:07 +0700

    Update main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#==============================================================================================
+'''
+Data Science Final Project Semester 5
+
+Members:
+- Radisa Hussien Rachmadi - 2301891752
+- Rayhan Ali Darmawan
+'''
+# IMPORTING LIBRARIES =========================================================================
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
