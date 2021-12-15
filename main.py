58385d7d2f548483e07c2da7a1758c0866171c8a
radisahussein <56021204+radisahussein@users.noreply.github.com>
2021-12-15 15:08:37 +0700

    data

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ import matplotlib.pyplot as plt
 
 # DATASETS ====================================================================================
 
-df_1 = pd.read_csv(r'C:\Users\Fajar Rachmadi\Desktop\CODE\DatSciFinalSem5\Data\conflict-data-for-indonesia-2.csv')
-df_2 = pd.read_csv(r'C:\Users\Fajar Rachmadi\Desktop\CODE\DatSciFinalSem5\Data\conflict_data_idn2.csv')
+df_1 = pd.read_csv(r'C:\Users\Fajar Rachmadi\Desktop\CODE\DatSciFinalSem5\Data\conflict_data_idn2 - conflict_data_idn2.csv')
+df_2 = pd.read_csv(r'C:\Users\Fajar Rachmadi\Desktop\CODE\DatSciFinalSem5\Data\conflict-data-for-indonesia-2 - conflict-data-for-indonesia-2.csv')
 
 # PREPARING DATA ====================================================================================================
 
@@ -23,3 +23,5 @@ df = pd.concat([df_1,df_2]).drop_duplicates().reset_index(drop=True)
 print(df.head(20))
 
 print(df.sort_values(by='year'))
+
+df.to_csv(r'C:\Users\Fajar Rachmadi\Desktop\CODE\DatSciFinalSem5\Data\final_data.csv',index=False)
